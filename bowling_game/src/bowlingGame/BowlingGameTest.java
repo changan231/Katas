a9dfe4e78e6dfca419baba5da0900e5f621ad1b9
changan.xia <changan231@gmail.com>
2019-01-21 22:54:25 +0800

    Make the second test case work.

--- bowling_game/src/bowlingGame/BowlingGameTest.java
+++ bowling_game/src/bowlingGame/BowlingGameTest.java
@@ -3,11 +3,25 @@ package bowlingGame;
 import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
-	public void testGutterGame() throws Exception {
-		Game g = new Game();
-		for (int i = 0; i < 20; i++) {
-			g.roll(0);
+	private Game g;
+
+	protected void setUp() throws Exception {
+		g = new Game();
+	}
+
+	private void rollMany(int n, int pins) {
+		for (int i = 0; i < n; i++) {
+			g.roll(pins);
 		}
+	}
+
+	public void testGutterGame() throws Exception {
+		rollMany(20, 0);
 		assertEquals(0, g.score());
+	}
+
+	public void testAllOnes() throws Exception {
+		rollMany(20, 1);
+		assertEquals(20, g.score());
 	}
 }
