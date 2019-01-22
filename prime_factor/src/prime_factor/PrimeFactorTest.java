f0136ea8cb77c9ed71d6203c4c42b8bc934b33af
changan.xia <changan231@gmail.com>
2019-01-22 22:41:56 +0800

    Make the second test case work (Prime Factor).

--- prime_factor/src/prime_factor/PrimeFactorTest.java
+++ prime_factor/src/prime_factor/PrimeFactorTest.java
@@ -1,5 +1,7 @@
 package prime_factor;
 
+import static prime_factor.PrimeFactors.generate;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,10 +9,17 @@ import junit.framework.TestCase;
 
 public class PrimeFactorTest extends TestCase {
 	public void testOne() throws Exception {
-		assertEquals(listPrimeFactors(), PrimeFactors.generate(1));
+		assertEquals(listPrimeFactors(), generate(1));
 	}
 
-	private List<Integer> listPrimeFactors() {
-		return new ArrayList<Integer>();
+	public void testTwo() throws Exception {
+		assertEquals(listPrimeFactors(2), generate(2));
+	}
+
+	private List<Integer> listPrimeFactors(int... ints) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i : ints)
+			list.add(i);
+		return list;
 	}
 }
