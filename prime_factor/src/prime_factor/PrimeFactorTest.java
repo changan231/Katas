c54187c38c55107fd1615b769b8c1dad1bad3be3
changan.xia <changan231@gmail.com>
2019-01-22 22:32:35 +0800

    Make fist test case work (Prime Factor).

--- prime_factor/src/prime_factor/PrimeFactorTest.java
+++ prime_factor/src/prime_factor/PrimeFactorTest.java
@@ -1,6 +1,16 @@
 package prime_factor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.TestCase;
 
 public class PrimeFactorTest extends TestCase {
+	public void testOne() throws Exception {
+		assertEquals(listPrimeFactors(), PrimeFactors.generate(1));
+	}
+
+	private List<Integer> listPrimeFactors() {
+		return new ArrayList<Integer>();
+	}
 }
---
+++ prime_factor/src/prime_factor/PrimeFactors.java
@@ -0,0 +1,12 @@
+package prime_factor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+
+	public static List<Integer> generate(int n) {
+		return new ArrayList<Integer>();
+	}
+
+}
